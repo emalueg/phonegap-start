2b191543f129acb7f0e3f5a3314032997794a7fe
emalueg <emalueg@infinitytechnology.com>
2012-05-11 09:13:26 -0500

    Update assets/js/app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -55,22 +55,6 @@ run(function () {
     });
     when('#test', function () {
     	alert("Test Function hit");
-        store.get('config', function (saved) {
-            // construct a gmap str
-            var map = saved ? saved.map || ui('map') : ui('map')
-            , zoom = saved ? saved.zoom || ui('zoom') : ui('zoom')
-            , path = "http://maps.google.com/maps/api/staticmap?center=";
-
-            navigator.geolocation.getCurrentPosition(function (position) {
-                var location = "" + position.coords.latitude + "," + position.coords.longitude;
-                path += location + "&zoom=" + zoom;
-                path += "&size=250x250&maptype=" + map + "&markers=color:red|label:P|";
-                path += location + "&sensor=false";
-
-                x$('img#test_map').attr('src', path);
-            }, function () {
-                x$('img#test_map').attr('src', "assets/img/gpsfailed.png");
-            });
         });
     });  
     when('#goback', function(){alert("go back");});
