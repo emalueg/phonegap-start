18a569be900efade09dba205ef1d4adb3ce461eb
emalueg <emalueg@infinitytechnology.com>
2012-05-16 15:45:43 -0500

    Update assets/js/dsl.js

--- assets/js/dsl.js
+++ assets/js/dsl.js
@@ -23,6 +23,7 @@ var run = function(application) {
 
 // reg a click to [id]_button, displays id (if it exists) and executes callback (if it exists)
 , when = function(id, callback) {
+		alert("when " + id);
     x$(id + '_button').on('touchstart', function () {
         if (x$(id).length > 0)
             display(id);
